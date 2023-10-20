71dfe75da055479866e6573ba33dd6c5b0d054bd
psk1730 <129039579+psk1730@users.noreply.github.com>
2023-10-20 18:05:55 +0000

    added dynamic data

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'data scientist',
+        'location': 'India Delhi',
+    },
+    {
+        'id': 2,
+        'title': 'data analyst',
+        'location': 'India Gurugram',
+    },
+    {
+        'id': 3,
+        'title': 'Web Developer',
+        'location': 'India Bengluru',
+    },
+    {
+        'id': 4,
+        'title': 'Electrical Engineer',
+        'location': 'India Pune',
+    },
+]
+
+
 @app.route("/")
 def hello_world():
-    return "<p>yes my replit is working  now </p>"
+  return render_template("home.html", var=JOBS, name="Pravesh")
 
-if __name__ == "__main__" :
-  app.run(host= '0.0.0.0', debug=True)
-  
+@app.route("/api/jobs")
+def H_world():
+  return jsonify(JOBS)
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
+
+{{ }}
